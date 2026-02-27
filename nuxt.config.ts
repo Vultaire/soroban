10fdcf58fff62d1ed131b2dedaadec30f1505286
Paul Goins <general@vultaire.net>
2026-02-26 22:31:20 -0800

    Tweaking for static build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  nitro: {
+    // Not even seeing this in Nitro's docs; ChatGpt/Google gave me this...
+    preset: "static",
+  },
   telemetry: false,
   ssr: false,
 })
